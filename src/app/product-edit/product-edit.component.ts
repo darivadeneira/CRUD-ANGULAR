115c2bce4c55e9625070aba4fb31916e7672aa43
unknown <darivadeneira7@espe.edu.ec>
2024-06-15 20:11:40 -0500

    CRUD completo y estilos

---
+++ src/app/product-edit/product-edit.component.ts
@@ -0,0 +1,38 @@
+import { Component, Inject } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Product } from '../../product.interface';
+
+@Component({
+  selector: 'app-product-edit',
+  standalone: true,
+  providers: [ProductService],
+  imports: [FormsModule],
+  templateUrl: './product-edit.component.html',
+  styleUrl: './product-edit.component.css',
+})
+export class ProductEditComponent {
+  product: Product = { id: '', name: '', price: 0 };
+
+  constructor(
+    private productService: ProductService,
+    @Inject(ActivatedRoute) private route: ActivatedRoute,
+    @Inject(Router) private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const id: string = this.route.snapshot.paramMap.get('id')!;
+    this.productService.getProduct(id).subscribe((data: Product) => {
+      this.product = data;
+    });
+    console.log(this.product);
+    
+  }
+
+  updateProduct(): void {
+    this.productService.updateProduct(this.product).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
+}
---
+++ src/app/product.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../product.interface';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+    private apiUrl = 'http://localhost:3000/products';
+
+    constructor(private http: HttpClient) {}
+
+    getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+    }
+
+    getProduct(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+    }
+
+    addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, product);
+    }
+
+    updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${product.id}`, product);
+    }
+
+    deleteProduct(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+}
